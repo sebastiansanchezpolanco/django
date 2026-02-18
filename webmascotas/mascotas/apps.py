3bd49c0981a445c08230e3fc8db70ddaa5e70469
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-02-18 19:23:01 +0100

    up

--- webmascotas/mascotas/apps.py
+++ webmascotas/mascotas/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class AplicacionConfig(AppConfig):
-    name = 'aplicacion'
+    name = 'mascotas'
--- webmascotas/webmascotas/urls.py
+++ webmascotas/webmascotas/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('aplicacion/', include("aplicacion.urls")),
+    path('mascotas/', include("mascotas.urls")),
     path('admin/', admin.site.urls),
 ]
