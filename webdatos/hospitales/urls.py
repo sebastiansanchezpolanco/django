dcf83d0c1d1644b024612a410273e29265249b33
sebastiansanchezpolaco <sebastiansanchezpolanco@gmail.com>
2026-02-24 10:50:21 +0100

    up

---
+++ webdatos/hospitales/urls.py
@@ -0,0 +1,11 @@
+from hospitales import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path("departamentos/", views.tabladepartamentos, name="departamentos"),
+    path("insertdept/", views.insertarDepartamento, name="insertdept"),
+    path("updatedept/", views.updateDepartamento, name="updatedept"),
+    path("buscarform/", views.buscarDepartamentoForm, name="buscarform"),
+    path("buscarget/", views.buscarDepartamentoGet, name="buscarget"),
+]
