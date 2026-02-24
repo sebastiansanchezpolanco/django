df355fb9a2c608021f66006ef2c2c24132ce5ad5
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-02-24 15:46:04 +0100

    up

--- webdatos/hospitales/models.py
+++ webdatos/hospitales/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+import oracledb
 
 # Create your models here.
+class Departamento:
+    def __init__(self):
+        self.id = 0
+        self.nombre = ""
+        self.localidad = ""
+
+class ServiceDepartamentos:
+    def __init__(self):
+        self.connection = oracledb.connect(user="system", password="oracle"
+                                           , dsn = "localhost/freepdb1")
+    
+    def getDepartamentos(self):
+        sql = "select * from DEPT"
+        cursor = self.connection.cursor()
+        cursor.execute(sql)
+        listaDepartamentos = []
+        for row in cursor:
+            dept = Departamento()
+            dept.id = row[0]
+            dept.nombre = row[1]
+            dept.localidad = row[2]
+            listaDepartamentos.append(dept)
+        cursor.close()
+        return listaDepartamentos
+    
+    def insertarDepartamento(self, id, nombre, loc):
+        sql = "insert into DEPT values (:id, :nom, :loc)"
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (id, nombre, loc,))
+        self.connection.commit()
+        cursor.close()  
+        
+    def updateDepartamento(self, id, nombre, localidad):
+        sql = "update DEPT set DNOMBRE=:nom, LOC=:loc where DEPT_NO=:id"
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (nombre, localidad, id,))
+        self.connection.commit()
+        cursor.close()
+    
+    #Vamos a crear un metodo para buscar un departamento por su ID
+    def buscarDepartamento(self, id):
+        sql = "select * from DEPT where DEPT_NO=:id"
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (id,))
+        row = cursor.fetchone()
+        dept = Departamento()
+        dept.id = row[0]
+        dept.nombre = row[1]
+        dept.localidad = row[2]
+        cursor.close()
+        return dept
--- webdatos/hospitales/views.py
+++ webdatos/hospitales/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+#Agregamos la librería de models.py
+from hospitales import models as md
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def tabladepartamentos(request):
+    #Creamos un servicio de departamentos
+    service = md.ServiceDepartamentos()
+    lista = service.getDepartamentos()
+    informacion = {
+        "departamentos": lista
+    }
+    return render (request, "departamentos.html", informacion)
+
+def insertarDepartamento(request):
+    if ('cajaid' in request.POST):
+        service = md.ServiceDepartamentos()
+        id = int(request.POST["cajaid"])
+        nom = request.POST["cajanombre"]
+        loc = request.POST["cajalocalidad"]
+        service.insertarDepartamento(id, nom, loc)
+        lista = service.getDepartamentos()
+        informacion = {
+            "departamentos": lista
+        }
+        return render (request, "departamentos.html", informacion)        
+    else:
+        return render(request, "insertardepartamento.html")
+
+def updateDepartamento(request):
+    if ('cajaid' in request.POST):
+        service = md.ServiceDepartamentos()
+        id = (int(request.POST["cajaid"]))
+        nom = request.POST["cajanombre"]
+        loc = request.POST["cajalocalidad"]
+        service.updateDepartamento(id, nom, loc)
+        lista = service.getDepartamentos()
+        informacion = {
+            "departamentos": lista
+        }
+        return render (request, "departamentos.html", informacion)
+    else:
+        return render(request, "updatedepartamento.html")
+
+def buscarDepartamentoForm(request):
+    if ('cajaid' in request.POST):
+        service = md.ServiceDepartamentos()
+        id = int(request.POST["cajaid"])
+        dept = service.buscarDepartamento(id)
+        informacion = {
+            "departamento": dept
+        }
+        return render(request, "buscarform.html", informacion)
+    else:
+        return render(request, "buscarform.html")
+    
+def buscarDepartamentoGet(request):
+    if ('dato' in request.GET):
+        service = md.ServiceDepartamentos()
+        id = int(request.GET["dato"])
+        dept = service.buscarDepartamento(id)
+        informacion = {
+            "departamento": dept
+        }
+        return render (request, "buscarget.html", informacion)
+    else:
+        return render (request, "buscarget.html")
