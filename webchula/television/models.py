eb40ab7bd603b454c6a12c112d12c523d87a9fe8
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-02-25 19:45:11 +0100

    up

--- webchula/television/models.py
+++ webchula/television/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+import oracledb 
 
 # Create your models here.
+class Serie:
+    def __init__(self):
+        self.idSerie=0
+        self.nombre=""
+        self.imagen=""
+        self.anyo=0
+
+class Personaje:
+    def __init__(self):
+        self.idPersonaje=0
+        self.nombre=""
+        self.imagen=""
+        self.idSerie=0
+
+class ServiceSeries:
+    def __init__(self):
+        self.connection=oracledb.connect(user="SYSTEM", password="oracle", dsn="localhost/FREEPDB1")
+    def getSeries(self):
+        sql="select * from SERIES"
+        cursor=self.connection.cursor()
+        cursor.execute(sql)
+        listaSeries=[]
+        for row in cursor:
+            serie=Serie()
+            serie.idSerie=row[0]
+            serie.nombre=row[1]
+            serie.imagen=row[2]
+            serie.anyo==row[3]
+            listaSeries.append(serie)
+        cursor.close() 
+        return listaSeries
+    def getPersonajesSeries(self, idserie):
+        sql="select * from PERSONAJES where IDSERIE=:id"
+        cursor=self.connection.cursor()
+        cursor.execute(sql, (idserie, ))
+        listaPersonajes=[]
+        for row in cursor:
+            personaje=Personaje()
+            personaje.idPersonaje=row[0]
+            personaje.nombre=row[1]
+            personaje.imagen=row[2]
+            personaje.idSerie=row[3]
+            listaPersonajes.append(personaje)
+        cursor.close() 
+        return listaPersonajes
+    def insertarPersonaje(self, personaje, imagen, idserie):
+        sql="select MAX(idpersonaje)+1 as maximo from PERSONAJES"
+        cursor = self.connection.cursor()
+        cursor.execute(sql)
+        idnuevopersonaje=int(cursor.fetchone()[0])
+        sql="insert into PERSONAJES (:idnuevopersonaje, :personaje, :imagen, :idserie)"
+        cursor = self.connection.cursor()
+        cursor.execute(sql, (personaje, imagen, idserie))
+        self.connection.commit()
+        cursor.close()  
+
--- webchula/television/urls.py
+++ webchula/television/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from television import views
 urlpatterns=[
     path("", views.index, name="index"),
+    path("series", views.series, name="series"),
+    path("personajes", views.personajes, name="personajes"),
+    path("insertarPersonaje", views.insertarPersonaje, name="insertarPersonaje"),
 ]
